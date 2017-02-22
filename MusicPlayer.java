d5966d184ab91b4a133170fd6da609b4735c4b41
Micah Chao <chaom1994@gmail.com>
2017-02-21 20:58:28 -0500

    First commit of JFugue

---
+++ MusicPlayer.java
@@ -0,0 +1,9 @@
+import org.jfugue.player.Player;
+
+public class MusicPlayer {
+  public static void main(String[] args) {
+    Player player = new Player();
+    player.play("V0 I[Piano] Bq Aq Gh | Bq Aq Gh | Gi Gi Gi Gi Ai Ai Ai Ai | Bq Aq Gh");
+    player.play("cq cq gq gq | aq aq gh | fq fq eq eq | dq dq ch | gq gq fq fq | eq eq dh | gq gq fq fq | eq eq dh | cq cq gq gq | aq aq gh | fq fq eq eq | dq dq ch");
+  }
+}
---
+++ makefile
@@ -0,0 +1,3 @@
+main:
+	javac -cp "./jfugue-5.0.7.jar" MusicPlayer.java
+	java -classpath ".:./jfugue-5.0.7.jar" MusicPlayer
